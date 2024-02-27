484d6a950945aa57b6d01d092c68dd379edc7dfa
Marcos <msoto@hopewellsystem.com>
2024-02-27 14:51:16 -0600

    M4 Loops t1 1

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,23 @@
 package school.mjc.stage0.loops.task1;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+    	int n = 20;
+    	String text = "writing the same code doesn't have much impact, and it's also time consuming \n";
+    		
+    String resultado = IntStream.range(0, n)
+    									.mapToObj(i -> text)
+    									.collect(Collectors.joining());
+    System.out.println(resultado);
+    }
+    
+    public static void main(String[] args) {
+    	RepeatIsBad repeat = new RepeatIsBad();
+    	repeat.repeatIsBad();
     }
 }
